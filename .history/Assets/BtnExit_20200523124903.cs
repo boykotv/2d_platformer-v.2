1fa384962501cdd53a4bc9857f8f5152d1a79cd6
titanya <tanya.titanya@gmail.com>
2020-05-23 12:51:30 +0300

    New unity version, Exit button

---
+++ .history/Assets/BtnExit_20200523124903.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnExit : MonoBehaviour
+{
+   public void doExit()
+   {
+       Debug.Log("Exit Game");
+       Application.Quit();
+   }
+}
